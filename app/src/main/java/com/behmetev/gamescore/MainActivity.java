e73411ef0b3e0c66916a7234e9c563f80fcd0538
Nikita Behmetev <nikitabehmetev@gmail.com>
2023-03-20 10:03:28 +0300

    Подстановка заначений при запуске приложения

--- app/src/main/java/com/behmetev/gamescore/MainActivity.java
+++ app/src/main/java/com/behmetev/gamescore/MainActivity.java
@@ -3,12 +3,22 @@ package com.behmetev.gamescore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int score1 = 0;
+    private int score2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textViewScore1 = findViewById(R.id.textViewScoreTeam1);
+        TextView textViewScore2 = findViewById(R.id.textViewScoreTeam2);
+
+        textViewScore1.setText("" + score1);
+        textViewScore1.setText("" + score2);
     }
 }
